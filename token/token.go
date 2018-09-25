b0193bf5ff70083fb70243671bd0422c3a9c3997
zhangqinyao <zhangqinyao@qiniu.com>
2018-09-25 17:41:46 +0800

    add support for if/else/true/false/eq/not_eq

--- token/token.go
+++ token/token.go
@@ -28,9 +28,18 @@ const (
 	LT       = "<"
 	GT       = ">"
 
+	TRUE  = "TRUE"
+	FALSE = "FALSE"
+
 	//Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+
+	EQ     = "=="
+	NOT_EQ = "!="
 )
 
 type TokenType string
@@ -41,8 +50,13 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
 }
 
 func LookupIdent(ident string) TokenType {
